6e4d9d22441aaaa3dafcfbc84480e58504fade3d
Steven <Steven@kynrai>
2014-07-31 23:55:19 +0100

    Fixed printer to print to console. Removed debug option to see output better

--- storm/Hello-Storm/src/main/java/storm/bolts/PrinterBolt.java
+++ storm/Hello-Storm/src/main/java/storm/bolts/PrinterBolt.java
@@ -1,35 +1,21 @@
 package storm.bolts;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-
 import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Tuple;
 
-public class PrinterBolt extends BaseBasicBolt{
+public class PrinterBolt extends BaseBasicBolt {
 
-	private static final long serialVersionUID = 2548849706249993565L;
-	File file;
+    private static final long serialVersionUID = 2548849706249993565L;
 
-	public void execute(Tuple input, BasicOutputCollector collector) {
-		
-		try {
-			file = new File("/home/dev/output.txt");
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
-			bw.write(input.toString());
-			bw.close();
-		} catch (Exception e) {
-			System.out.print(e.getMessage());
-		}
-	}
+    public void execute(Tuple input, BasicOutputCollector collector) {
+        
+        System.out.println(input.getIntegerByField("number"));
+    }
 
-	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		
-	}
-	
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+    }
 
 }
